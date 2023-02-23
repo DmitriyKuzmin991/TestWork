46cd4c821f761a259f4d41e992886ebf3e8d6848
DmitriyKuzmin991 <121133178+DmitriyKuzmin991@users.noreply.github.com>
2023-02-23 15:54:31 +0300

    Get data from console

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Написать программу, которая из имеющегося массива строк 
+// формирует массив из строк длинна которых меньше или равна 3 символа.
+// массив можно задать с клавиатуры.
+
+System.Console.WriteLine("Введите данные для массива через пробел");
+String writeData = Console.ReadLine();
+
+
+
